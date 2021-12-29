b8def801addb35ad70c571b6127a74a48e0e3cd8
Anton Cristo <antoncristo92@gmail.com>
2021-12-29 15:18:47 +0200

    update api key

--- src/constants/weather-api-constants.ts
+++ src/constants/weather-api-constants.ts
@@ -1,4 +1,4 @@
-export const API_KEY = "QzNYxOsmDtMbqTSKYkGzlk5EGNt6ZK0O";
+export const API_KEY = "Z8AzhopWPYN27BR7zqVFSotNuOX5KvPk";
 
 export const AUTOCOMPLETE_REQUEST_URL =
   "https://dataservice.accuweather.com/locations/v1/cities/autocomplete";
